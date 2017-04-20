61d12f4c7dd0e92f20a0dd49bfbdf314113eb440
takayukioda <takayuki@odashi.me>
2017-04-20 18:50:28 +0900

    refact: add message comment in the code, and update test code

--- app/main.js
+++ app/main.js
@@ -1,6 +1,13 @@
 "use strict";
 
 function main(argv) {
+  /**
+   * このコードは引数と標準出力を用いたサンプルコードです。
+   * このコードは好きなように編集・削除してもらって構いません。
+   *
+   * This is a sample code to use arguments and outputs.
+   * You can edit and even delte this code as you like.
+   */
   argv.forEach((v, i) => console.log(`argv[${i}]: ${v}`));
 }
 
